5b48b3d4c0325d4cc344cda392342bc0a74b3306
Lucas Stadler <lu@papill0n.org>
2015-03-18 18:18:54 +0100

    move the index to its own package as well

---
+++ index/main.go
@@ -0,0 +1,117 @@
+package index
+
+import (
+	"github.com/heyLu/fressian"
+	"log"
+
+	"../storage"
+)
+
+var readHandlers = map[string]fressian.ReadHandler{
+	"index-root-node": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
+		tData, _ := r.ReadObject()
+		directories, _ := r.ReadObject()
+		return IndexRootNode{
+			nil,
+			tData.(IndexTData),
+			directories.([]interface{}),
+		}
+	},
+	"index-tdata": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
+		vs, _ := r.ReadObject()
+		es, _ := r.ReadObject()
+		as, _ := r.ReadObject()
+		txs, _ := r.ReadObject()
+		addeds, _ := r.ReadObject()
+		return IndexTData{
+			vs.([]interface{}),
+			es.([]int),
+			as.([]int),
+			txs.([]int),
+			addeds.([]bool),
+		}
+	},
+	"index-dir-node": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
+		tData, _ := r.ReadObject()
+		segments, _ := r.ReadObject()
+		mystery1, _ := r.ReadObject()
+		mystery2, _ := r.ReadObject()
+		return IndexDirNode{
+			tData.(IndexTData),
+			segments.([]interface{}),
+			mystery1.([]int), mystery2.([]int),
+		}
+	},
+}
+
+type IndexRootNode struct {
+	store       *storage.Store
+	tData       IndexTData
+	directories []interface{}
+}
+
+type IndexDirNode struct {
+	tData    IndexTData
+	segments []interface{}
+	mystery1 []int
+	mystery2 []int
+}
+
+type IndexTData struct {
+	values       []interface{}
+	entities     []int
+	attributes   []int
+	transactions []int
+	addeds       []bool
+}
+
+type Datom struct {
+	entity      int
+	attribute   int
+	value       interface{}
+	transaction int
+	added       bool
+}
+
+func (root *IndexRootNode) Datoms() []Datom {
+	datoms := make([]Datom, 0, 1000)
+	store := root.store
+	for _, dirId := range root.directories {
+		dir, err := storage.Get(store, dirId.(string), readHandlers)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, segmentId := range dir.(IndexDirNode).segments {
+			segmentRaw, err := storage.Get(store, segmentId.(string), readHandlers)
+			if err != nil {
+				log.Fatal(err)
+			}
+			segment := segmentRaw.(IndexTData)
+			for i, _ := range segment.entities {
+				datom := Datom{
+					segment.entities[i],
+					segment.attributes[i],
+					segment.values[i],
+					3*(1<<42) + segment.transactions[i],
+					segment.addeds[i],
+				}
+				datoms = append(datoms, datom)
+			}
+		}
+	}
+	return datoms
+}
+
+type Index interface {
+	Datoms() []Datom
+}
+
+func New(store *storage.Store, id string) (Index, error) {
+	indexRaw, err := storage.Get(store, id, readHandlers)
+	if err != nil {
+		return nil, err
+	}
+	indexRoot := indexRaw.(IndexRootNode)
+	indexRoot.store = store
+	return Index(&indexRoot), nil
+}
--- mu.go
+++ mu.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"os"
 
+	"./index"
 	"./storage"
 )
 
@@ -23,117 +24,8 @@ func Connect(u *url.URL) (*Connection, error) {
 	return &Connection{store}, nil
 }
 
-var readHandlers = map[string]fressian.ReadHandler{
-	"index-root-node": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
-		tData, _ := r.ReadObject()
-		directories, _ := r.ReadObject()
-		return IndexRootNode{
-			nil,
-			tData.(IndexTData),
-			directories.([]interface{}),
-		}
-	},
-	"index-tdata": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
-		vs, _ := r.ReadObject()
-		es, _ := r.ReadObject()
-		as, _ := r.ReadObject()
-		txs, _ := r.ReadObject()
-		addeds, _ := r.ReadObject()
-		return IndexTData{
-			vs.([]interface{}),
-			es.([]int),
-			as.([]int),
-			txs.([]int),
-			addeds.([]bool),
-		}
-	},
-	"index-dir-node": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
-		tData, _ := r.ReadObject()
-		segments, _ := r.ReadObject()
-		mystery1, _ := r.ReadObject()
-		mystery2, _ := r.ReadObject()
-		return IndexDirNode{
-			tData.(IndexTData),
-			segments.([]interface{}),
-			mystery1.([]int), mystery2.([]int),
-		}
-	},
-}
-
-type IndexRootNode struct {
-	store       *storage.Store
-	tData       IndexTData
-	directories []interface{}
-}
-
-type IndexDirNode struct {
-	tData    IndexTData
-	segments []interface{}
-	mystery1 []int
-	mystery2 []int
-}
-
-type IndexTData struct {
-	values       []interface{}
-	entities     []int
-	attributes   []int
-	transactions []int
-	addeds       []bool
-}
-
-func (root *IndexRootNode) Datoms() []Datom {
-	datoms := make([]Datom, 0, 1000)
-	store := root.store
-	for _, dirId := range root.directories {
-		dir, err := storage.Get(store, dirId.(string), readHandlers)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, segmentId := range dir.(IndexDirNode).segments {
-			segmentRaw, err := storage.Get(store, segmentId.(string), readHandlers)
-			if err != nil {
-				log.Fatal(err)
-			}
-			segment := segmentRaw.(IndexTData)
-			for i, _ := range segment.entities {
-				datom := Datom{
-					segment.entities[i],
-					segment.attributes[i],
-					segment.values[i],
-					3*(1<<42) + segment.transactions[i],
-					segment.addeds[i],
-				}
-				datoms = append(datoms, datom)
-			}
-		}
-	}
-	return datoms
-}
-
-type Index interface {
-	Datoms() []Datom
-}
-
-func NewIndex(store *storage.Store, id string) (Index, error) {
-	indexRaw, err := storage.Get(store, id, readHandlers)
-	if err != nil {
-		return nil, err
-	}
-	indexRoot := indexRaw.(IndexRootNode)
-	indexRoot.store = store
-	return Index(&indexRoot), nil
-}
-
-type Datom struct {
-	entity      int
-	attribute   int
-	value       interface{}
-	transaction int
-	added       bool
-}
-
 type Database struct {
-	eavt Index
+	eavt index.Index
 }
 
 func (c *Connection) Db() (*Database, error) {
@@ -143,7 +35,7 @@ func (c *Connection) Db() (*Database, error) {
 	}
 	indexRoot := indexRootRaw.(map[interface{}]interface{})
 	eavtId := indexRoot[fressian.Key{"", "eavt-main"}].(string)
-	eavt, err := NewIndex(c.store, eavtId)
+	eavt, err := index.New(c.store, eavtId)
 	if err != nil {
 		return nil, err
 	}
