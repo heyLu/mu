5c8b7c2f500019dea78f0c0ace0bd768400b6e2b
Lucas Stadler <lu@papill0n.org>
2015-03-19 16:58:35 +0100

    find the root automatically if there is only one

--- storage/main.go
+++ storage/main.go
@@ -33,7 +33,17 @@ func Open(u *url.URL) (*Store, error) {
 	baseDir := u.Path
 	rootId := u.Query().Get("root")
 	if rootId == "" {
-		return nil, errors.New("must specify a ?root=<root> parameter")
+		roots, err := listDir(path.Join(baseDir, "roots"))
+		if err != nil {
+			return nil, err
+		}
+
+		if len(roots) == 0 {
+			return nil, errors.New("invalid dir, no roots")
+		} else if len(roots) > 1 {
+			return nil, errors.New("multiple roots, must specify a ?root=<root> parameter")
+		}
+		rootId = roots[0]
 	}
 	p := path.Join(baseDir, "roots", rootId)
 	f, err := os.Open(p)
@@ -47,6 +57,14 @@ func Open(u *url.URL) (*Store, error) {
 	root := rootRaw.(map[interface{}]interface{})
 	indexRootId := root[fressian.Key{"index", "root-id"}].(string)
 	return &Store{baseDir, indexRootId}, nil
+}
+
+func listDir(path string) ([]string, error) {
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return dir.Readdirnames(-1)
 }
 
 func Get(s *Store, id string, handlers map[string]fressian.ReadHandler) (interface{}, error) {
