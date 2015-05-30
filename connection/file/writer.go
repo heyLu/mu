99a057d1ef0cc1bc94d09b2a5e15736fec91c8cd
Lucas Stadler <lu@papill0n.org>
2015-05-30 18:21:41 +0200

    start maintaining avet and vaet indexes as well
    vaet is always empty for now, avet only contains `:db/ident` mappings.

--- connection/file/writer.go
+++ connection/file/writer.go
@@ -12,7 +12,7 @@ import (
 var WriteHandler fressian.WriteHandler = func(w *fressian.Writer, val interface{}) error {
 	switch val := val.(type) {
 	case *database.Database:
-		return w.WriteExt("mu.Database", val.Eavt(), val.Aevt(), nil, nil)
+		return w.WriteExt("mu.Database", val.Eavt(), val.Aevt(), val.Avet(), val.Vaet())
 	default:
 		return memory.WriteHandler(w, val)
 	}
@@ -22,13 +22,17 @@ var ReadHandlers = map[string]fressian.ReadHandler{
 	"mu.Database": func(r *fressian.Reader, tag string, fieldCount int) interface{} {
 		eavtRaw, _ := r.ReadValue()
 		aevtRaw, _ := r.ReadValue()
-		r.ReadValue()
-		r.ReadValue()
+		avetRaw, _ := r.ReadValue()
+		vaetRaw, _ := r.ReadValue()
 		eavt := eavtRaw.(*memory.Index)
 		eavt.UseCompare(index.CompareEavt)
 		aevt := aevtRaw.(*memory.Index)
 		aevt.UseCompare(index.CompareAevt)
-		return database.New(eavt, aevt, nil, nil)
+		avet := avetRaw.(*memory.Index)
+		avet.UseCompare(index.CompareAvet)
+		vaet := vaetRaw.(*memory.Index)
+		vaet.UseCompare(index.CompareVaet)
+		return database.New(eavt, aevt, avet, vaet)
 	},
 	"mu.memory.Index":   memory.ReadHandlers["mu.memory.Index"],
 	"mu.Datom":          index.ReadHandlers["mu.Datom"],
--- connection/memory/main.go
+++ connection/memory/main.go
@@ -20,7 +20,9 @@ type Connection struct {
 func New(u *url.URL) (connection.Connection, error) {
 	eavt := memoryIndex.New(index.CompareEavt)
 	aevt := memoryIndex.New(index.CompareAevt)
-	db := database.New(eavt, aevt, nil, nil)
+	avet := memoryIndex.New(index.CompareAvet)
+	vaet := memoryIndex.New(index.CompareVaet)
+	db := database.New(eavt, aevt, avet, vaet)
 	return &Connection{db}, nil
 }
 
@@ -37,6 +39,28 @@ func (c *Connection) TransactDatoms(datoms []index.Datom) error {
 	eavt = eavt.AddDatoms(datoms)
 	aevt := c.db.Aevt().(*memoryIndex.Index)
 	aevt = aevt.AddDatoms(datoms)
-	c.db = database.New(eavt, aevt, nil, nil)
+	avet := c.db.Avet().(*memoryIndex.Index)
+	avet = avet.AddDatoms(filterDatoms(needsAvet, datoms))
+	vaet := c.db.Vaet().(*memoryIndex.Index)
+	vaet = vaet.AddDatoms(filterDatoms(needsVaet, datoms))
+	c.db = database.New(eavt, aevt, avet, vaet)
 	return nil
+}
+
+func needsAvet(datom index.Datom) bool {
+	return datom.Attribute() == 10 // db/ident
+}
+
+func needsVaet(datom index.Datom) bool {
+	return false
+}
+
+func filterDatoms(pred func(index.Datom) bool, datoms []index.Datom) []index.Datom {
+	filteredDatoms := make([]index.Datom, 0, len(datoms))
+	for _, datom := range datoms {
+		if pred(datom) {
+			filteredDatoms = append(filteredDatoms, datom)
+		}
+	}
+	return filteredDatoms
 }
