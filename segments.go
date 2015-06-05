41a7027cb43ee5102d4376bfbef471e801267826
Lucas Stadler <lu@papill0n.org>
2015-06-05 17:23:41 +0200

    more thoughts on segments, caching and stores

--- segments.go
+++ segments.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 type Datom struct{}
 type IndexRoot struct{}
 type Log struct{}
-type Db struct{}
+
+//type Db struct{}
 
 // The in-memory index is supposed to merge the log with the data
 // in the persisted index.  The Datomic docs state that it's
@@ -94,6 +99,151 @@ type SegmentedIndex interface {
 	// - only needs to provide access to the most recent db
 	MergeLog(log Log) (database Db)
 }
+
+type Root struct {
+	tData       TransposedData
+	directories []string
+}
+
+type Directory struct {
+	tData    TransposedData
+	segments []string
+	mystery1 []int
+	mystery2 []int
+}
+
+type TransposedData struct {
+	values       []interface{}
+	entities     []int
+	attributes   []int
+	transactions []int
+	addeds       []bool
+}
+
+// The (global) object cache
+//
+// Individual stores check the cache before retrieving an object from
+// the cache.  When it is missing they get it from storage and put it
+// in the cache, returning *the same value* that is returned when it
+// is returned by the cache.
+//
+// The cache is a LRU cache, which removes old objects.
+// (http://godoc.org/github.com/golang/groupcache/lru seems suitable.)
+//
+// cache.Cache is the global cache instance in the `cache` package.
+//
+// Code using either the storage *or* the cache should not need to
+// cast values retrieved from it.  The cache should probably do the
+// casting, that way each store does not have to do it.
+var cache Cache
+
+type Cache interface {
+	// This seems non-ideal, because the cache shouldn't know too
+	// much about the data, and because we'd need to do basically
+	// the same action for `Root`s and `Directory`s, just with a
+	// different cast.
+	//
+	// However, where *should* we do it?
+	GetTransposedData(store Store, id string) *TransposedData
+}
+
+type globalCache struct {
+	cache map[string]interface{}
+}
+
+func (c *globalCache) GetTransposedData(store Store, id string) *TransposedData {
+	if val, ok := c.cache[id]; ok {
+		// This... doesn't seem right.  Should we store these in different caches?
+		// But that would only make this *one place* nicer, and make restricting
+		// the cache size more difficult.
+		return val.(*TransposedData)
+	}
+
+	log.Printf("[cache] get tdata segment %s from store\n", id)
+	_, err := store.Get(id)
+	if err != nil {
+		log.Fatal("[cache] get from store: ", err)
+		return nil
+	}
+
+	// FIXME: parse data
+	var tData *TransposedData = nil
+	c.cache[id] = tData
+
+	return tData
+}
+
+type Store interface {
+	Get(id string) ([]byte, error)
+	Put(id string, data []byte) error
+	Delete(id string) error
+}
+
+type memoryStore struct {
+	store map[string][]byte
+}
+
+func (s *memoryStore) Get(id string) ([]byte, error) {
+	if data, ok := s.store[id]; ok {
+		return data, nil
+	}
+
+	return nil, fmt.Errorf("No such object: %s", id)
+}
+
+func (s *memoryStore) Put(id string, data []byte) error {
+	s.store[id] = data
+	return nil
+}
+
+func (s *memoryStore) Delete(id string) error {
+	delete(s.store, id)
+	return nil
+}
+
+type fileStore struct {
+	path string
+}
+
+func (s fileStore) blobPath(id string) string {
+	return path.Join(s.path, id[len(id)-2:], id)
+}
+
+func (s fileStore) Get(id string) ([]byte, error) {
+	return ioutil.ReadFile(s.blobPath(id))
+}
+
+func (s fileStore) Put(id string, data []byte) error {
+	return ioutil.WriteFile(s.blobPath(id), data, 0644)
+}
+
+func (s fileStore) Delete(id string) error {
+	return os.Remove(s.blobPath(id))
+}
+
+type Db struct {
+	eavt Index
+	aevt Index
+	avet Index
+	vaet Index
+}
+
+// Could be purely in-memory or segmented, or an index
+// that mixes these.  (I.e. when there are new datoms that are
+// not yet indexed.
+type Index interface {
+	DatomsAt(start, end Datom) Iterator
+}
+
+type Iterator interface {
+	Next() *Datom
+}
+
+// DatomsAt returns an iterator for the datoms between start and end
+//
+// - figures out the segment where the start datom should be
+// - when it hits the end of the segment, it either goes on to the next
+// - or it goes one level up and down again
 
 func main() {
 	fmt.Println(".")
