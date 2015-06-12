f0dcda86dbb863a52166522da74972e5e83b23e6
Lucas Stadler <lu@papill0n.org>
2015-06-13 00:03:23 +0200

    introduce an alternative to datomic's datoms function
    we want typed patterns, so we a) use the type checker and b) don't have
    to specify the index, because the pattern implies it.

--- mu.go
+++ mu.go
@@ -2,12 +2,14 @@ package mu
 
 import (
 	"github.com/heyLu/fressian"
+	"log"
 	"net/url"
 
 	"./connection"
 	_ "./connection/backup"
 	_ "./connection/file"
 	_ "./connection/memory"
+	"./database"
 	"./index"
 	"./transactor"
 )
@@ -49,6 +51,97 @@ func Retraction(datom index.Datom) transactor.Datum {
 
 func Datums(datoms ...transactor.TxDatum) []transactor.TxDatum {
 	return datoms
+}
+
+type DatomPattern struct {
+	idx index.Type
+	n   int
+	e   transactor.TxLookup
+	a   transactor.Keyword
+	v   index.Value
+}
+
+func E(entity transactor.TxLookup) DatomPattern {
+	return DatomPattern{idx: index.Eavt, n: 1, e: entity}
+}
+
+func Ea(entity transactor.TxLookup, attribute transactor.Keyword) DatomPattern {
+	return DatomPattern{idx: index.Eavt, n: 2, e: entity, a: attribute}
+}
+
+func Eav(entity transactor.TxLookup, attribute transactor.Keyword, value interface{}) DatomPattern {
+	return DatomPattern{idx: index.Eavt, n: 3, e: entity, a: attribute, v: index.NewValue(value)}
+}
+
+func A(attribute transactor.Keyword) DatomPattern {
+	return DatomPattern{idx: index.Aevt, n: 1, a: attribute}
+}
+
+func Ae(attribute transactor.Keyword, entity transactor.TxLookup) DatomPattern {
+	return DatomPattern{idx: index.Aevt, n: 2, a: attribute, e: entity}
+}
+
+func Aev(attribute transactor.Keyword, entity transactor.TxLookup, value interface{}) DatomPattern {
+	return DatomPattern{idx: index.Aevt, n: 3, a: attribute, e: entity, v: index.NewValue(value)}
+}
+
+func Datoms(db *database.Db, pattern DatomPattern) (index.Iterator, error) {
+	min := index.MinDatom
+	max := index.MaxDatom
+
+	minE, maxE := min.E(), max.E()
+	minA, maxA := min.A(), max.A()
+	minV, maxV := index.MinValue, index.MaxValue
+
+	var idx index.Index
+
+	switch pattern.idx {
+	case index.Eavt:
+		if pattern.n == 1 {
+			e, err := pattern.e.Lookup(db)
+			if err != nil {
+				return nil, err
+			}
+			minE, maxE = e, e
+		}
+		if pattern.n == 2 {
+			a, err := pattern.e.Lookup(db)
+			if err != nil {
+				return nil, err
+			}
+			minA, maxA = a, a
+		}
+		if pattern.n == 3 {
+			minV, maxV = pattern.v, pattern.v
+		}
+		idx = db.Eavt()
+	case index.Aevt:
+		if pattern.n == 1 {
+			a, err := pattern.a.Lookup(db)
+			if err != nil {
+				return nil, err
+			}
+			minA, maxA = a, a
+		}
+		if pattern.n == 2 {
+			e, err := pattern.e.Lookup(db)
+			if err != nil {
+				return nil, err
+			}
+			minE, maxE = e, e
+		}
+		if pattern.n == 3 {
+			minV, maxV = pattern.v, pattern.v
+		}
+		idx = db.Aevt()
+	default:
+		log.Fatal("invalid index type")
+		return nil, nil
+	}
+
+	return idx.DatomsAt(
+		index.NewDatom(minE, minA, minV, min.Tx(), min.Added()),
+		index.NewDatom(maxE, maxA, maxV, max.Tx(), max.Added())), nil
 }
 
 func Keyword(namespace, name string) fressian.Keyword {
