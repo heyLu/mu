6fd7bda011cf3fd4c4d4f4eaf03f9fde883d8c41
Lucas Stadler <lu@papill0n.org>
2015-07-09 17:28:12 +0200

    Implement join key getters with the new types

--- cmd/playground/joins.go
+++ cmd/playground/joins.go
@@ -4,7 +4,30 @@ type indexed interface {
 	valueAt(idx int) value
 }
 
+type tuple []value
+
+func (t tuple) valueAt(idx int) value { return t[idx] }
+
 type value interface{}
+
+type variable interface{}
+
+func getterFn(attrs map[variable]int, attr variable) func(tuple) value {
+	idx := attrs[attr]
+	return func(tuple tuple) value {
+		return tuple.valueAt(idx)
+	}
+}
+
+func hashKeyFn(getters ...func(tuple) value) func(tuple) indexed {
+	return func(tuple tuple) indexed {
+		vals := make([]value, len(getters))
+		for i, getter := range getters {
+			vals[i] = getter(tuple)
+		}
+		return newHashKey(vals)
+	}
+}
 
 func main() {
 	var val value
@@ -15,6 +38,20 @@ func main() {
 	println()
 	m := map[indexed][]value{}
 	println(m)
+}
+
+func newHashKey(vals []value) indexed {
+	switch len(vals) {
+	case 1:
+		return key1{val1: vals[0]}
+	case 2:
+		return key2{val1: vals[0], val2: vals[1]}
+	case 3:
+		return key3{val1: vals[0], val2: vals[1],
+			val3: vals[2]}
+	default:
+		panic("unsupported join arity")
+	}
 }
 
 type key1 struct{ val1 value }
