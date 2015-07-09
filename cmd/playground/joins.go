7a1b97659fed235b76fbb8b63f84dba68b5ce517
Lucas Stadler <lu@papill0n.org>
2015-07-09 18:08:04 +0200

    Implement hashJoin again

--- cmd/playground/joins.go
+++ cmd/playground/joins.go
@@ -16,6 +16,11 @@ type value interface{}
 
 type variable interface{}
 
+type relation struct {
+	attrs  map[variable]int
+	tuples []tuple
+}
+
 func getterFn(attrs map[variable]int, attr variable) func(tuple) value {
 	idx := attrs[attr]
 	return func(tuple tuple) value {
@@ -44,6 +49,76 @@ func hashAttrs(keyFn func(tuple) indexed, tuples []tuple) map[indexed][]tuple {
 		}
 	}
 	return m
+}
+
+func intersectKeys(attrs1, attrs2 map[variable]int) []variable {
+	keys := make([]variable, 0)
+	for attr1, _ := range attrs1 {
+		if _, ok := attrs2[attr1]; ok {
+			keys = append(keys, attr1)
+		}
+	}
+	return keys
+}
+
+// joinTuples returns a new tuple with the values from tuple1 and tuple2 at indexes idxs1 and idxs2, respectively.
+func joinTuples(tuple1 tuple, idxs1 []int, tuple2 tuple, idxs2 []int) tuple {
+	l1 := len(tuple1)
+	l2 := len(tuple2)
+	newTuple := make(tuple, l1+l2)
+	for i, idx := range idxs1 {
+		newTuple[i] = tuple1.valueAt(idx)
+	}
+	for i, idx := range idxs2 {
+		newTuple[l1+i] = tuple2.valueAt(idx)
+	}
+	return newTuple
+}
+
+func hashJoin(rel1, rel2 relation) relation {
+	// join on the attributes both have in common
+	commonAttrs := intersectKeys(rel1.attrs, rel2.attrs)
+	commonGetters1 := make([]func(tuple) value, len(commonAttrs))
+	commonGetters2 := make([]func(tuple) value, len(commonAttrs))
+	for i, attr := range commonAttrs {
+		commonGetters1[i] = getterFn(rel1.attrs, attr)
+		commonGetters2[i] = getterFn(rel2.attrs, attr)
+	}
+	// take all attributes from rel1
+	keepAttrs1 := make([]variable, len(rel1.attrs))
+	keepIdxs1 := make([]int, len(rel1.attrs))
+	i := 0
+	for attr, idx := range rel1.attrs {
+		keepAttrs1[i] = attr
+		keepIdxs1[i] = idx
+		i += 1
+	}
+	// only keep attrs not in rel1 from rel2
+	keepAttrs2 := make([]variable, 0)
+	keepIdxs2 := make([]int, 0)
+	for attr, idx := range rel2.attrs {
+		if _, ok := rel1.attrs[attr]; !ok {
+			keepAttrs2 = append(keepAttrs2, attr)
+			keepIdxs2 = append(keepIdxs2, idx)
+		}
+	}
+	// construct functions to get the "join key" from tuple
+	keyFn1 := hashKeyFn(commonGetters1...)
+	keyFn2 := hashKeyFn(commonGetters2...)
+	// collect tuples from rel1 by "join key"
+	hash := hashAttrs(keyFn1, rel1.tuples)
+	// join tuples with a matching join key
+	newTuples := make([]tuple, 0)
+	for _, tuple2 := range rel2.tuples {
+		key := keyFn2(tuple2)
+		if tuples1, ok := hash[key]; ok {
+			for _, tuple1 := range tuples1 {
+				joinTuple := joinTuples(tuple1, keepIdxs1, tuple2, keepIdxs2)
+				newTuples = append(newTuples, joinTuple)
+			}
+		}
+	}
+	return relation{}
 }
 
 func main() {
