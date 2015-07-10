fcf71c3f7e80f74f89c591942941966d1f1b6083
Lucas Stadler <lu@papill0n.org>
2015-07-10 15:34:57 +0200

    Move join keys into their own file
    This will make it easier to generate that file later on.  In addition,
    this way these definitions don't clutter the main query file.

--- query/query.go
+++ query/query.go
@@ -380,64 +380,6 @@ func newVar(namespace string, name ...string) variable {
 	}
 }
 
-// newHashKey returns a value implementing indexed that contains
-// the given values.
-//
-// For now the maximum number of values supported is three.  This limit
-// is arbitrary and can easily be changed.  We will change it based on
-// how much joins queries need in practice.
-func newHashKey(vals []value) indexed {
-	switch len(vals) {
-	case 1:
-		return key1{val1: vals[0]}
-	case 2:
-		return key2{val1: vals[0], val2: vals[1]}
-	case 3:
-		return key3{val1: vals[0], val2: vals[1], val3: vals[2]}
-	default:
-		panic("unsupported join arity")
-	}
-}
-
-type key1 struct{ val1 value }
-
-func (j key1) valueAt(idx int) value {
-	switch idx {
-	case 1:
-		return j.val1
-	default:
-		panic("invalid index")
-	}
-}
-
-type key2 struct{ val1, val2 value }
-
-func (j key2) valueAt(idx int) value {
-	switch idx {
-	case 1:
-		return j.val1
-	case 2:
-		return j.val2
-	default:
-		panic("invalid index")
-	}
-}
-
-type key3 struct{ val1, val2, val3 value }
-
-func (j key3) valueAt(idx int) value {
-	switch idx {
-	case 1:
-		return j.val1
-	case 2:
-		return j.val2
-	case 3:
-		return j.val3
-	default:
-		panic("invalid index")
-	}
-}
-
 type Query struct{}
 
 func Q(query Query, inputs ...interface{}) (interface{}, error) {
