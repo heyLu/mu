028b379df49c27c87a3acc597484b27d198f5b89
Lucas Stadler <lu@papill0n.org>
2015-10-01 12:52:11 +0200

    Add a `bolt` store using boltdb
    This is one option for single-file stores.  It is not included by
    default, to keep the dependency count low.  To make this store
    availlable, one has to import it.

---
+++ store/bolt/bolt.go
@@ -0,0 +1,109 @@
+package bolt
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/heyLu/mu/store"
+)
+
+func init() {
+	store.Register("bolt", create, open)
+}
+
+func create(u *url.URL) (bool, error) {
+	path := u.Host + u.Path
+
+	isNew := false
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			isNew = true
+		} else {
+			return false, err
+		}
+	}
+	f.Close()
+
+	db, err := bolt.Open(path, 0600, &bolt.Options{
+		Timeout: 1 * time.Second,
+	})
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("mu_kvs"))
+		return err
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return isNew, nil
+}
+
+func open(u *url.URL) (store.Store, error) {
+	path := u.Host + u.Path
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	f.Close()
+
+	db, err := bolt.Open(path, 0600, &bolt.Options{
+		Timeout: 1 * time.Second,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &boltStore{db}, nil
+}
+
+type boltStore struct {
+	db *bolt.DB
+}
+
+func (s *boltStore) Get(id string) ([]byte, error) {
+	var data []byte
+	fmt.Println("get", id)
+	err := s.db.View(func(tx *bolt.Tx) error {
+		data = tx.Bucket([]byte("mu_kvs")).Get([]byte(id))
+		if data == nil {
+			return fmt.Errorf("key does not exist")
+		}
+		fmt.Println(data)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+	return data, nil
+}
+
+func (s *boltStore) Put(id string, data []byte) error {
+	fmt.Println("put", id, data)
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket([]byte("mu_kvs")).Put([]byte(id), data)
+	})
+	return err
+}
+
+func (s *boltStore) Delete(id string) error {
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket([]byte("mu_kvs")).Delete([]byte(id))
+	})
+	return err
+}
+
+func (s *boltStore) Close() error {
+	return s.db.Close()
+}
