a05fa00cafc8dbc5def28a9d7ac33e360c77856d
Lucas Stadler <lu@papill0n.org>
2015-10-09 10:06:09 +0200

    Implement .Reverse on the retraction-filtering iterator
    Not entirely sure that this will work, but it seems to work for simple
    indexes.

--- database/iter.go
+++ database/iter.go
@@ -33,5 +33,39 @@ func (i *noRetractionsIterator) Next() *index.Datom {
 }
 
 func (i *noRetractionsIterator) Reverse() index.Iterator {
+	return &reverseNoRetractionsIterator{iter: i.iter.Reverse()}
+}
+
+type reverseNoRetractionsIterator struct {
+	iter  index.Iterator
+	datom *index.Datom
+}
+
+func (i *reverseNoRetractionsIterator) Next() *index.Datom {
+	for {
+		// the index is reversed, so we always need to check for two
+		// datoms at once.
+		datom1 := i.datom
+		if datom1 == nil {
+			datom1 = i.iter.Next()
+		}
+		datom2 := i.iter.Next()
+
+		if datom1 == nil && datom2 == nil { // at end, no more values
+			return nil
+		} else if datom2 == nil { // only one more value
+			i.datom = nil
+			return datom1
+		} else if !datom2.Added() { // retraction, skip two
+			i.datom = nil
+			continue
+		} else { // no retraction, return first, store second
+			i.datom = datom2
+			return datom1
+		}
+	}
+}
+
+func (i *reverseNoRetractionsIterator) Reverse() index.Iterator {
 	panic("not implemented")
 }
